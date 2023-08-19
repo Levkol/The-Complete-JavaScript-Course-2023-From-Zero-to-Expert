5bff22c88e4af66e163248e6d2aa803b81610459
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-08-19 20:48:58 +0200

    Setter and Getter

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -100,32 +100,69 @@ BMW.accelerate();
 
 // class declaration
 class PersonCl2 {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
-  // Methods will be added to .prototype property
-  calcAge() {
-    console.log(2037 - this.birthYear);
+  //   // Methods will be added to .prototype property
+  //   calcAge() {
+  //     console.log(2037 - this.birthYear);
+  //   }
+
+  //   greet() {
+  //     console.log(`Hey ${this.firstName}`);
+  //   }
+
+  get age() {
+    return 2037 - this.birthYear;
   }
 
-  greet() {
-    console.log(`Hey ${this.firstName}`);
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(` `)) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl2(`Jessica`, 1996);
+const jessica = new PersonCl2(`Jessica Davis`, 1996);
 console.log(jessica);
-jessica.calcAge();
+// jessica.calcAge();
+console.log(jessica.age);
+console.log(jessica.fullName);
 
 console.log(jessica.__proto__ === PersonCl2.prototype);
 
 // PersonCl2.prototype.greet = function () {
 //   console.log(`Hey ${this.firstName}`);
 // };
-jessica.greet();
+// jessica.greet();
 
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizes
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCl2(`Walter White`, 1965);
+
+const account = {
+  owner: `Jonas`,
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
