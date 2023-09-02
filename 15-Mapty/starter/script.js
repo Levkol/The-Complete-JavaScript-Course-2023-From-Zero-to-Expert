9122e2c3e1c11635e19340a0a69be948e2cf6da4
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-09-02 12:21:50 +0200

    Move to Marker On Click

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -4,6 +4,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + ``).slice(-10)
+  clicks = 0;
 
   constructor(coords, distance, duration){
     this.coords = coords; // [lat, lng]
@@ -16,6 +17,10 @@ class Workout {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
+
+  click(){
+    this.clicks++;
   }
 }
 
@@ -68,6 +73,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #Workouts = [];
 
@@ -75,6 +81,7 @@ class App {
     this._getPosition();
     form.addEventListener(`submit`, this._newWorkout.bind(this));
     inputType.addEventListener(`change`, this._toggleElevationField);
+    containerWorkouts.addEventListener(`click`, this._moveToPoup.bind(this));
   }
 
   _getPosition() {
@@ -95,7 +102,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -258,6 +265,26 @@ class App {
     `;
 
     form.insertAdjacentHTML(`afterend`, html);
+  }
+
+  _moveToPoup(e){
+    const workoutEl = e.target.closest(`.workout`);
+    console.log(workoutEl);
+
+    if(!workoutEl) return;
+
+    const workout = this.#Workouts.find(work => work.id === workoutEl.dataset.id);
+    console.log(workout);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // Using the public interface
+    workout.click();
   }
 }
 
