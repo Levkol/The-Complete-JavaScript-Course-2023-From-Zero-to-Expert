47fca973121958d796ccfa6d2e279d80c8f41b22
Levente Nagy <nagy46levente@gmail.com>
2023-09-04 18:09:16 +0200

    Consuming Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -106,9 +106,20 @@ const renderCountry = function(data, className){
 
 ///////////
 
-// const request = new XMLHttpRequest();
-// request.open(`GET`, `https://restcountries.com/v2/name/${country}`);
-// request.send();
+// const getCountryData = function(country){
+//     fetch(`https://restcountries.com/v2/name/${country}`).then(function(response){
+//         console.log(response);
+//         return response.json();
+//     }).then(function(data){
+//         console.log(data);
+//         renderCountry(data[0]);
+//     })
+// };
 
-const request = fetch(`https://restcountries.com/v2/name/portugal`);
-console.log(request);
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v2/name/${country}`).then(response => 
+    response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData(`portugal`);
