2e169b66bbdf0e5fede9af8127d669472f5bdff0
Levente Nagy <nagy46levente@gmail.com>
2023-09-04 18:21:16 +0200

    Chaining Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -117,9 +117,18 @@ const renderCountry = function(data, className){
 // };
 
 const getCountryData = function(country){
-    fetch(`https://restcountries.com/v2/name/${country}`).then(response => 
-    response.json())
-    .then(data => renderCountry(data[0]));
+    // Country 1
+    fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+        renderCountry(data[0]);
+        const neighbour = data[0].borders?.[0];
+
+        // Country 2
+        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+        .then(response => response.json())
+        .then(data => renderCountry(data, `neighbour`));
+        });
 };
 
 getCountryData(`portugal`);
