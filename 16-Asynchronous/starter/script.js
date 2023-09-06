51bfb427944431db4a81bed1da068a8627afb7e7
Levente Nagy <nagy46levente@gmail.com>
2023-09-06 13:03:40 +0200

    Building a Simple Promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -239,6 +239,7 @@ whereAmI(-33.933, 18.474);
 // Promise.resolve(`Resolve promise 1`).then(res => console.log(res)); // 3
 // console.log(`Test end`); // 2
 
+/*
 console.log(`Test start`); // 1
 setTimeout(() => console.log(`0 sec timer`), 0); // 5
 Promise.resolve(`Resolve promise 1`).then(res => console.log(res)); // 3
@@ -249,3 +250,57 @@ Promise.resolve(`Resolve promise 2`).then(res => {
 }); // 4
 
 console.log(`Test end`); // 2
+*/
+
+// Building a Promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log(`Lottery draw is happening 🔮`);
+
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve(`You WIN 🥇`);
+    } else {
+      reject(new Error(`You lost your money 💩`));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log(`1 second passed`);
+    return wait(1);
+  })
+  .then(() => {
+    console.log(`2 second passed`);
+    return wait(1);
+  })
+  .then(() => {
+    console.log(`3 second passed`);
+    return wait(1);
+  })
+  .then(() => console.log(`4 second passed`));
+
+// setTimeout(() => {
+//     console.log(`1 second passed`);
+//     setTimeout(() => {
+//         console.log(`2 seconds passed`);
+//         setTimeout(() => {
+//             console.log(`3 seconds passed`);
+//             setTimeout(() => {
+//                 console.log(`4 seconds passed`);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+Promise.resolve(`abc`).then(x => console.log(x));
+Promise.reject(new Error(`Problem!`)).catch(x => console.error(x));
