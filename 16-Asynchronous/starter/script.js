6171823c9b38a4fbf4c90501057c06c5e11ffb2e
Levente Nagy <nagy46levente@gmail.com>
2023-09-07 07:47:06 +0200

    Coding Challenge #2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -307,6 +307,7 @@ Promise.resolve(`abc`).then(x => console.log(x));
 Promise.reject(new Error(`Problem!`)).catch(x => console.error(x));
 */
 
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     // navigator.geolocation.getCurrentPosition(
@@ -353,3 +354,51 @@ const whereAmI = function () {
 };
 
 btn.addEventListener(`click`, whereAmI);
+*/
+
+//////////////////////////////////// Coding Challenge 2 ////////////////////////////////////
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(`.images`);
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement(`img`);
+    img.src = imgPath;
+
+    img.addEventListener(`load`, function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener(`error`, function () {
+      reject(new Error(`Image not found`));
+    });
+  });
+};
+
+let currentImg;
+
+createImage(`img/img-1.jpg`)
+  .then(img => {
+    currentImg = img;
+    console.log(`Image 1 loaded`);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = `none`;
+    return createImage(`img/img-2.jpg`);
+  })
+  .then(img => {
+    currentImg = img;
+    console.log(`Image 2 loaded`);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = `none`;
+  })
+  .catch(err => console.error(err));
