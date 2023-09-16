bff883527f4584956151067fe1f14874094d7d48
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-09-16 08:28:44 +0200

    Coding Challenge #3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -741,56 +741,65 @@ createImage(`img/img-1.jpg`)
   .catch(err => console.error(err));
   */
 
-  /////////////////////////// Coding Challenge 3://////////////////////////
-  const wait = function (seconds) {
-    return new Promise(function (resolve) {
-      setTimeout(resolve, seconds * 1000);
+/////////////////////////// Coding Challenge 3://////////////////////////
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(`.images`);
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement(`img`);
+    img.src = imgPath;
+
+    img.addEventListener(`load`, function () {
+      imgContainer.append(img);
+      resolve(img);
     });
-  };
-  
-  const imgContainer = document.querySelector(`.images`);
-  
-  const createImage = function (imgPath) {
-    return new Promise(function (resolve, reject) {
-      const img = document.createElement(`img`);
-      img.src = imgPath;
-  
-      img.addEventListener(`load`, function () {
-        imgContainer.append(img);
-        resolve(img);
-      });
-  
-      img.addEventListener(`error`, function () {
-        reject(new Error(`Image not found`));
-      });
+
+    img.addEventListener(`error`, function () {
+      reject(new Error(`Image not found`));
     });
-  };
-  
- ////////////////////////////// 1. Part //////////////////////////////
-  
-  const loadNPause = async function(){
-    try{
-      // Image 1 load
-      let img = await createImage(`img/img-1.jpg`)
-      console.log(`Image 1 loaded`);
-      await wait(2);
-      img.style.display = `none`;
+  });
+};
 
-      // Image 2 load
-      img = await createImage(`img/img-2.jpg`)
-      console.log(`Image 2 loaded`);
-      await wait(2);
-      img.style.display = `none`;
+////////////////////////////// 1. Part //////////////////////////////
 
-    } catch (err) {
-      console.error(`${err} 💥`)
-    }
-  };
+const loadNPause = async function () {
+  try {
+    // Image 1 load
+    let img = await createImage(`img/img-1.jpg`);
+    console.log(`Image 1 loaded`);
+    await wait(2);
+    img.style.display = `none`;
 
-  loadNPause();
+    // Image 2 load
+    img = await createImage(`img/img-2.jpg`);
+    console.log(`Image 2 loaded`);
+    await wait(2);
+    img.style.display = `none`;
+  } catch (err) {
+    console.error(`${err} 💥`);
+  }
+};
 
-  // const loadAll = async function(...imgArr){
-  //   console.log(imgArr);
-  //   imgArr.map( img => createImage(img[0], createImage(img[1]), createImage(img[2])))
-  // }
-  // loadAll([`img/img-1.jpg`, `img/img-2.jpg`, `img/img-3.jpg`]);
+// loadNPause();
+
+////////////////////////////// 2. Part //////////////////////////////
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+
+    imgsEl.forEach(img => img.classList.add(`parallel`));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll([`img/img-1.jpg`, `img/img-2.jpg`, `img/img-3.jpg`]);
