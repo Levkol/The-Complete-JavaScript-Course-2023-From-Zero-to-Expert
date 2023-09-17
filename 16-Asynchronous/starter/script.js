4987ba59548707f745b2250c91a3bcc74e3df2b2
Levente Nagy <nagy46levente@gmail.com>
2023-09-17 16:53:12 +0200

    Committing local changes before merging

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -692,6 +692,7 @@ Coordinates 1: 52.508, 13.381 (Latitude, Longitude)
 */
 
 /////////////////////////// Coding Challenge 2://////////////////////////
+/*
 
 const wait = function (seconds) {
   return new Promise(function (resolve) {
@@ -738,3 +739,67 @@ createImage(`img/img-1.jpg`)
     currentImg.style.display = `none`;
   })
   .catch(err => console.error(err));
+  */
+
+/////////////////////////// Coding Challenge 3://////////////////////////
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(`.images`);
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement(`img`);
+    img.src = imgPath;
+
+    img.addEventListener(`load`, function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener(`error`, function () {
+      reject(new Error(`Image not found`));
+    });
+  });
+};
+
+////////////////////////////// 1. Part //////////////////////////////
+
+const loadNPause = async function () {
+  try {
+    // Image 1 load
+    let img = await createImage(`img/img-1.jpg`);
+    console.log(`Image 1 loaded`);
+    await wait(2);
+    img.style.display = `none`;
+
+    // Image 2 load
+    img = await createImage(`img/img-2.jpg`);
+    console.log(`Image 2 loaded`);
+    await wait(2);
+    img.style.display = `none`;
+  } catch (err) {
+    console.error(`${err} 💥`);
+  }
+};
+
+// loadNPause();
+
+////////////////////////////// 2. Part //////////////////////////////
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+
+    imgsEl.forEach(img => img.classList.add(`parallel`));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll([`img/img-1.jpg`, `img/img-2.jpg`, `img/img-3.jpg`]);
