731b3f94e3771a8a0c743a721f0493eeea70dbca
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-09-17 12:01:26 +0200

    CommonJS Moduls

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -54,7 +54,7 @@ console.log(lastPost2);
 */
 
 ///////////////////// Modul Pattern
-
+/*
 const ShoppingCart2 = (function(){
     const cart = [];
     const shippingCost = 10;
@@ -82,3 +82,15 @@ ShoppingCart2.addToCart(`apple`, 4);
 ShoppingCart2.addToCart(`pizza`, 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+*/
+
+///////////////////// CommonJS Moduls
+
+// Export
+export.addToCart = function(product, quantity){
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+};
+
+// Import
+const { addToCart } = requrie('./shoppingCart.js')
